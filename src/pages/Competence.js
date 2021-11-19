cbe5fda367e82b0d7c8412acc30290f7a37a3701
Piett-rm <rmpetit24@gmail.com>
2021-11-19 21:40:00 +0100

    Home (Hero Section)

---
+++ src/pages/Competence.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Competence = () => (
+  <div>
+    <h1>This is Competence</h1>
+  </div>
+);
+
+export default Competence;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeroSection from '../compoments/HeroSection';
+
+const Home = () => (
+  <div>
+    <HeroSection />
+  </div>
+);
+
+export default Home;
